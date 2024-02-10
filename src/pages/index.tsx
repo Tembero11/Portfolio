10193da345e192862d6854a5b8b5dd095a88fd49
Tembero11 <silver.teemu@gmail.com>
2024-02-10 13:42:38 +0000

    Add site structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,36 @@
 import * as React from "react"
 import type { HeadFC, PageProps } from "gatsby"
 import Layout from "../components/Layout/Layout"
+import * as styles from "../styles/pages/index.module.scss"
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <Layout>
-      <p>Hello from index</p>
+      {/* Intro */}
+      <section className={styles.section}>
+        <div>
+          <h1 style={{marginTop: 0}}>Hello there! <br/> I'm Teemu Silver</h1>
+          <p>A self-taught software developer with <strong>7 years</strong> of <br /> programming as a hobbist under my belt.</p>
+          <button>Get in touch</button>
+        </div>
+      </section>
+      {/* Work Experience */}
+      <section className={styles.section} data-direction="row">
+        <div>
+          <h1>Work Experience</h1>
+          <p>This is some little speech about my work</p>
+        </div>
+        <p>Here are gonna be some boxes</p>
+      </section>
+      {/* Tech */}
+      <section className={styles.section}>
+        <h1>Tech</h1>
+        <button>See More</button>
+      </section>
+      {/* Projects */}
+      <section className={styles.section}>
+        <h1>Projects</h1>
+      </section>
     </Layout>
   )
 }
